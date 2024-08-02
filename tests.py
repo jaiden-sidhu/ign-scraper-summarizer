78fbce25375be74c9dec4f5b112f0067c70c8ace
jaiden-sidhu <jaiden.s.sidhu@gmail.com>
2024-08-01 23:24:45 -0500

    Added a tests class

---
+++ tests.py
@@ -0,0 +1,21 @@
+from main import *
+
+# Make sure to comment out the tests you aren't running, it'll take a while to run every test :)
+
+# Should generate no result
+main("jkbfjkasbdjkbbdiabsd")
+
+# Should generate a positive review for Red Dead Redemption 2
+main("red dead 2")
+
+# Should generate a negative review for Sonic Unleashed
+main("sonic unleashed")
+
+# Should generate a conflicted review for Sonic '06
+main("sonic the hedgehog 2006")
+
+# Should generate a positive review for Mario 3D Land
+main("mario 3d land")
+
+# Should generate a neutral review for Sonic and the Black Knight
+main("sonic black knight")
